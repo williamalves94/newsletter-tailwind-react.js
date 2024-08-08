0cd14ae23d203082104da62ed2aad1c5ea915bcc
William Alves <williamcostacardoso94@gmail.com>
2024-08-07 21:02:39 -0300

    feat: criação dos componentes da função

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red">test</h1>
-      <Form />
-    </>
+    <div className="bg-slate-900 min-h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="font-bold text-white text-[2rem]">Inscreva-se</h1>
+      <p className="text-white">
+        Assine nossa Newsletter e mantenha-se informado
+      </p>
+      <div className="w-96 mt-4 bg-stone-200 px-4 py-4 rounded-lg">
+        <Form />
+      </div>
+      <p className="text-slate-100 text-xs w-96 mt-2 text-center">
+        Ao se inscrever, voce passará a receber os nossos e-mails com as
+        melhores dicas, novidades e ofertas.
+      </p>
+    </div>
   );
 }
 
